6a723a36df2d79d048dbb3b8354bce02a1651be6
Raza Ul Kareem <83599698+rez4president@users.noreply.github.com>
2021-05-24 09:51:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var user = {
+	name : "John",
+	age: 34,
+	place : "Mexico",
+	gender : "Male",
+	hobbies: ["Eating", "sleeping", "coding", "driving"],
+	talent : function(){
+		console.log("Laaaaa deeee daaaaa");
+	}
+};
